62da3ca82db3fb071c5852bd2f5fe01472980ef2
arjun <arjunpatidar952@gmail.com>
2018-03-24 20:47:18 +0530

    Factorial example using Recursion in c language

---
+++ C-programs/Factorial.c
@@ -0,0 +1,15 @@
+//
+// Created by arjun patidar on 24/03/18.
+//
+
+#include <stdio.h>
+long fact(int n ){
+    if(n>0)
+        return (n*fact(n-1));
+    else
+        return 1;
+
+}
+main(){
+    printf("Factorial of 5 is %d",fact(5));
+}
